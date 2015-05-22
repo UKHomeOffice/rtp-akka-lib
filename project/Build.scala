f2a9bbe6cf2c9bf2446569f0f28c750bf87d39d7
David Ainslie <dainslie@gmail.com>
2015-05-22 11:32:38 +0100

    REPL update

--- project/Build.scala
+++ project/Build.scala
@@ -20,7 +20,8 @@ object Build extends Build {
           "-language:existentials",
           "-language:reflectiveCalls",
           "-language:postfixOps",
-          "-Yrangepos"),
+          "-Yrangepos",
+          "-Yrepl-sync"),
         ivyScala := ivyScala.value map { _.copy(overrideScalaVersion = true) },
         resolvers ++= Seq(
           "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
