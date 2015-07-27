fb821815964aa5fd1c901686e6608cbc68707826
David Ainslie <dainslie@gmail.com>
2015-07-27 13:46:27 +0100

    Marshal refactor (could possibly use pickling)

--- src/main/scala/uk/gov/homeoffice/spray/Marshallers.scala
+++ src/main/scala/uk/gov/homeoffice/spray/Marshallers.scala
@@ -6,48 +6,49 @@ import scala.util.{Failure, Success}
 import spray.http.MediaTypes._
 import spray.http.StatusCodes._
 import spray.http.{HttpEntity, HttpResponse}
-import spray.httpx.marshalling.ToResponseMarshaller
+import spray.httpx.marshalling.{ToResponseMarshallingContext, ToResponseMarshaller}
 import org.json4s.JValue
 import org.json4s.native.Serialization.write
 import org.scalactic.{Bad, Good, Or}
 import grizzled.slf4j.Logging
 import uk.gov.homeoffice.json.{JsonError, JsonFormats}
 import org.json4s.native.JsonMethods._
+import scala.pickling.Defaults._, scala.pickling.json._
 
 /**
  * Implicit responses for JsonError are of type <anything> Or JsonError i.e. if not using custom response handling code and expecting the implicit functionality of this trait to be used, a response must match one of the declared marshallers here.
  * So the "orMarshaller" can handle either a Good(<anything>) or a Bad(JsonError).
  */
 trait Marshallers extends JsonFormats with Logging {
+  val  marshall: ToResponseMarshallingContext => PartialFunction[_ Or JsonError, Unit] = ctx => {
+    case Good(j: JValue) =>
+      println(s"===> Marshalling: ${compact(render(j))}")
+      ctx.marshalTo(HttpResponse(status = OK, entity = HttpEntity(`application/json`, compact(render(j)))))
+
+    /*case Good(g: AnyRef) =>
+      println(s"===> Marshalling: g")
+      ctx.marshalTo(HttpResponse(status = OK, entity = HttpEntity(`application/json`, g.pickle.toString)))*/
+
+    case Good(g: AnyRef) =>
+      println(s"===> Marshalling: g")
+      ctx.marshalTo(HttpResponse(status = OK, entity = HttpEntity(`application/json`, write(g))))
+
+    case Good(g) =>
+      println(s"===> Marshalling: g")
+      ctx.marshalTo(HttpResponse(status = OK, entity = HttpEntity(`text/plain`, g.toString)))
+
+    case Bad(jsonError) =>
+      ctx.marshalTo(HttpResponse(status = UnprocessableEntity, entity = HttpEntity(`application/json`, write(jsonError))))
+  }
+
   implicit val orMarshaller = ToResponseMarshaller.of[_ Or JsonError](`application/json`) { (value, contentType, ctx) =>
-    value match {
-      case Good(j: JValue) =>
-        println(s"===> Marshalling: ${compact(render(j))}")
-        ctx.marshalTo(HttpResponse(status = OK, entity = HttpEntity(`application/json`, compact(render(j)))))
-
-      case Good(g) =>
-        println(s"===> Marshalling: g")
-        ctx.marshalTo(HttpResponse(status = OK, entity = HttpEntity(`text/plain`, g.toString)))
-
-      case Bad(jsonError) =>
-        ctx.marshalTo(HttpResponse(status = UnprocessableEntity, entity = HttpEntity(`application/json`, write(jsonError))))
-    }
+    marshall(ctx)(value)
   }
 
   implicit val futureOrMarshaller = ToResponseMarshaller.of[Future[_ Or JsonError]](`application/json`) { (value, contentType, ctx) =>
     value.onComplete {
-      case Success(v) => v match {
-        case Good(j: JValue) =>
-          println(s"===> Marshalling: ${compact(render(j))}")
-          ctx.marshalTo(HttpResponse(status = OK, entity = HttpEntity(`application/json`, compact(render(j)))))
-
-        case Good(g) =>
-          println(s"===> Marshalling: g")
-          ctx.marshalTo(HttpResponse(status = OK, entity = HttpEntity(`text/plain`, g.toString)))
-
-        case Bad(jsonError) =>
-          ctx.marshalTo(HttpResponse(status = UnprocessableEntity, entity = HttpEntity(`application/json`, write(jsonError))))
-      }
+      case Success(v) =>
+        marshall(ctx)(v)
 
       case Failure(e) =>
         error(e)
