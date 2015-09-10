b9cb49624d12b432ccf5d7470b3660be4df59f57
David Ainslie <dainslie@gmail.com>
2015-09-10 16:20:35 +0100

    Formatting

--- src/test/scala/uk/gov/homeoffice/spray/RouteSpecification.scala
+++ src/test/scala/uk/gov/homeoffice/spray/RouteSpecification.scala
@@ -1,18 +1,18 @@
 package uk.gov.homeoffice.spray
 
+import scala.concurrent.duration._
 import akka.actor.ActorSystem
+import akka.testkit._
 import spray.httpx.Json4sSupport
 import spray.routing.HttpService
 import spray.testkit.{RouteTest, Specs2RouteTest}
 import org.specs2.mutable.Specification
 import uk.gov.homeoffice.json.JsonFormats
-import concurrent.duration._
-import akka.testkit._
 
 trait RouteSpecification extends Specification with Specs2RouteTest with HttpService with JsonFormats with Json4sSupport {
   this: RouteTest =>
 
-  implicit def default(implicit system: ActorSystem) = RouteTestTimeout(5.seconds dilated)
-
   val actorRefFactory = system
+
+  implicit def default(implicit system: ActorSystem) = RouteTestTimeout(5.seconds dilated)
 }
