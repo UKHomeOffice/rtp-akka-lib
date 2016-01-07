9d4c8285702aa38baf08e1cc18c5ddcbeee2aaa9
David Ainslie <dainslie@gmail.com>
2016-01-07 12:33:27 +0000

    Scheduling throws bog standard Throwable to allow supervising actors take appropriate action.

--- src/main/scala/akka/Scheduling.scala
+++ src/main/scala/akka/Scheduling.scala
@@ -3,8 +3,7 @@ package akka
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.duration._
-import scala.util.Try
-import akka.actor.{ActorLogging, Actor}
+import akka.actor.{Actor, ActorLogging}
 
 trait Scheduling[R] extends ActorLogging {
   this: Actor =>
@@ -20,15 +19,20 @@ trait Scheduling[R] extends ActorLogging {
       log.debug("Woken up")
       context.system.eventStream.publish(Scheduled(self.path))
 
-      Try {
+      try {
         scheduled match {
           case r: Future[_] if schedule.awaitOnFutures && schedule.scheduleAfterSuccess => r onComplete { _ => doSchedule(schedule.delay) }
           case _ if schedule.scheduleAfterSuccess => doSchedule(schedule.delay) // TODO Scheduled result of say Seq[Future], then await each Future if configured
+          case _ =>
         }
-      } recover {
+      } catch {
         case t: Throwable if schedule.scheduleAfterError =>
           log.error(s"Scheduling caused an exception which is being IGNORED and so this actor will not bubble up the error to its supervisor: $t")
           doSchedule(schedule.delay)
+
+        case t: Throwable =>
+          log.error(s"Exception in scheduling")
+          throw t
       }
   }
 
