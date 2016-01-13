6a1491da7f8a71282657bdb7c3f1e3e85a1d966a
David Ainslie <dainslie@gmail.com>
2016-01-13 18:01:38 +0000

    Version 1.2.0

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      version := "1.2.0-SNAPSHOT",
+      version := "1.2.0",
       scalaVersion := "2.11.7",
       scalacOptions ++= Seq(
         "-feature",
@@ -65,8 +65,8 @@ object Build extends Build {
       libraryDependencies ++= Seq(
         "uk.gov.homeoffice" %% "rtp-test-lib" % "1.0" withSources(),
         "uk.gov.homeoffice" %% "rtp-test-lib" % "1.0" % Test classifier "tests" withSources(),
-        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.0" withSources(),
-        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.0" % Test classifier "tests" withSources()
+        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.1.0" withSources(),
+        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.1.0" % Test classifier "tests" withSources()
       ))
   }
 }
