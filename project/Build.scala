3bca93a14afb1ae5358931cbb842e762f217854d
David Ainslie <dainslie@gmail.com>
2016-03-16 16:23:06 +0000

    Upgraded to Scala 2.11.8

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,7 @@ object Build extends Build {
       name := moduleName,
       organization := "uk.gov.homeoffice",
       version := "1.5.0-SNAPSHOT",
-      scalaVersion := "2.11.7",
+      scalaVersion := "2.11.8",
       scalacOptions ++= Seq(
         "-feature",
         "-language:implicitConversions",
