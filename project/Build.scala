f8772adf8e56adff36497d6580fee72e54171666
David Ainslie <dainslie@gmail.com>
2016-03-17 14:55:09 +0000

    Bumped version to 1.6.0

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      version := "1.5.0",
+      version := "1.6.0",
       scalaVersion := "2.11.8",
       scalacOptions ++= Seq(
         "-feature",
@@ -36,7 +36,7 @@ object Build extends Build {
     .settings(libraryDependencies ++= {
       val `akka-version` = "2.4.0"
       val `spray-version` = "1.3.3"
-      val `rtp-io-lib-version` = "1.6.0"
+      val `rtp-io-lib-version` = "1.7.0"
       val `rtp-test-lib-version` = "1.2.0"
 
       Seq(
