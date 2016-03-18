57affbb82aeb7cc24823119898714fb76c5dfb6a
David Ainslie <dainslie@gmail.com>
2016-03-18 16:42:34 +0000

    Version 1.6.2

--- project/Build.scala
+++ project/Build.scala
@@ -12,7 +12,7 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      version := "1.6.1",
+      version := "1.6.2",
       scalaVersion := "2.11.8",
       scalacOptions ++= Seq(
         "-feature",
@@ -36,7 +36,7 @@ object Build extends Build {
     .settings(libraryDependencies ++= {
       val `akka-version` = "2.4.0"
       val `spray-version` = "1.3.3"
-      val `rtp-io-lib-version` = "1.7.1"
+      val `rtp-io-lib-version` = "1.7.2"
       val `rtp-test-lib-version` = "1.2.1"
 
       Seq(
