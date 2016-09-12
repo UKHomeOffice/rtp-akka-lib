e413c2b5d20b829c8ab449fc013372089fb6b428
David Ainslie <dainslie@gmail.com>
2016-09-12 22:02:47 +0100

    WIP

--- src/main/scala/uk/gov/homeoffice/akka/cluster/ClusterActorSystem.scala
+++ src/main/scala/uk/gov/homeoffice/akka/cluster/ClusterActorSystem.scala
@@ -1,65 +1,153 @@
 package uk.gov.homeoffice.akka.cluster
 
 import scala.collection.JavaConversions._
+import scala.util.Try
 import akka.actor.ActorSystem
-import com.typesafe.config.{Config, ConfigFactory, ConfigList}
+import com.typesafe.config.{Config, ConfigFactory}
 import grizzled.slf4j.Logging
 
+/**
+  * Create an Actor System that can be clustered by enhancing a given application Config.
+  * The given config just has to provide seed nodes of the following format:
+  * <pre>
+  *   akka {
+  *     cluster {
+  *       seed-nodes = [{
+  *         host = "127.0.0.1"
+  *         host = ${?CLUSTER_SEED_NODE_HOST_1} # If using environment variables
+  *
+  *         port = 2661
+  *         port = ${?CLUSTER_SEED_NODE_PORT_1}
+  *       }, {
+  *         host = "127.0.0.1"
+  *         host = ${?CLUSTER_SEED_NODE_HOST_2}
+  *
+  *         port = 2662
+  *         port = ${?CLUSTER_SEED_NODE_PORT_2}
+  *       }]
+  *     }
+  *   }
+  *
+  *   Or maybe
+  *
+  *   akka {
+  *     cluster {
+  *       seed-nodes {
+  *         1 {
+  *           host = "127.0.0.1"
+  *           host = ${?CLUSTER_SEED_NODE_HOST_1} # If using environment variables
+  *
+  *           port = 2661
+  *           port = ${?CLUSTER_SEED_NODE_PORT_1}
+  *         }
+  *
+  *         2 {
+  *           host = "127.0.0.1"
+  *           host = ${?CLUSTER_SEED_NODE_HOST_2}
+  *
+  *           port = 2662
+  *           port = ${?CLUSTER_SEED_NODE_PORT_2}
+  *         }
+  *       }
+  *     }
+  *   }
+  * </pre>
+  */
 object ClusterActorSystem extends Logging {
-  def apply(name: String, config: Config, node: Int = sys.props("cluster.node").toInt) =
-    ActorSystem(name, clusterConfig(name, config, node))
+  /**
+    * Create an Actor System as a seed node of a cluster.
+    * @param name String The name of the actor system where each node of a cluster must have the same name.
+    * @param config Config Where this object enhances the given Config with cluster configuration.
+    *               This given config can override anything configured by this object.
+    * @param node Int Representing the node number of the seed nodes e.g. node 2 of 3 seed nodes.
+    *             Client code will probably want this to be given as an environment variable e.g. -Dcluster.node=1, captured in code as sys.props("cluster.node").toInt
+    * @return ActorSystem
+    */
+  def apply(name: String, config: Config, node: Int): ActorSystem = {
+    def createClusterConfig: Config = {
+      val seedNodes: Seq[String] = clusterSeedNodes(name, config)
+      info(s"Cluster node $node with seed nodes: ${seedNodes.mkString(", ")}")
 
-  def clusterConfig(name: String, config: Config, node: Int): Config = {
-    val seedNodes: ConfigList = config.getList("akka.cluster.seed-nodes")
-    info(s"Cluster seed nodes configuration: $seedNodes")
+      val HostPort = """.*?@(.*?):(\d*).*""".r
+      val HostPort(host, port) = seedNodes.get(node - 1)
+      info(s"Configuring node $node on $host:$port in cluster '$name'")
 
-    val seedNode = seedNodes.get(node - 1).atKey("seed-node")
-    val host = seedNode.getString("seed-node.host")
-    val port = seedNode.getInt("seed-node.port")
-    info(s"Configuring cluster node $node on $host:$port")
+      clusterConfig(name, config, host, port.toInt, seedNodes)
+    }
 
-    ConfigFactory.parseString(s"""
-      akka {
-        actor {
-          provider = "akka.cluster.ClusterActorRefProvider"
-        }
-
-        remote {
-          enabled-transports = ["akka.remote.netty.tcp"]
-          log-remote-lifecycle-events = off
-
-          netty {
-            tcp {
-              hostname = "$host"
-              port = $port
-            }
-          }
-        }
-
-        cluster {
-          seed-nodes = [
-            ${
-              seedNodes map { _.atKey("seed-node") } map { seedNode =>
-                val host = seedNode.getString("seed-node.host")
-                val port = seedNode.getInt("seed-node.port")
-                s""""akka.tcp://$name@$host:$port""""
-              } mkString ", "
-            }
-          ]
-
-          min-nr-of-members = 2
-          auto-down-unreachable-after = 30s
-
-          metrics {
-            enabled = off
-          }
-        }
-
-        extensions = [
-          "akka.cluster.pubsub.DistributedPubSub",
-          "akka.cluster.metrics.ClusterMetricsExtension"
-        ]
-      }
-    """).withFallback(config)
+    ActorSystem(name, createClusterConfig)
   }
+
+  /**
+    * Create an Actor System to dynamically add to cluster seed nodes.
+    * @param name String The name of the actor system where each node of a cluster must have the same name.
+    * @param config Config Where this object enhances the given Config with cluster configuration.
+    *               This given config can override anything configured by this object.
+    * @param host String Of the host to dynamically add to the seed cluster nodes.
+    *             Client code will probably want this to be given as an environment variable e.g. -Dcluster.host=rtp.homeoffice.gov.uk, captured in code as sys.props("cluster.host")
+    * @param port Int Of the port for the given host to dynamically add to the seed cluster nodes.
+    *             Client code will probably want this to be given as an environment variable e.g. -Dcluster.port=2665, captured in code as sys.props("cluster.port").toInt
+    * @return ActorSystem
+    */
+  def apply(name: String, config: Config, host: String, port: Int): ActorSystem = {
+    def createClusterConfig: Config = {
+      val seedNodes: Seq[String] = clusterSeedNodes(name, config)
+      info(s"Cluster dynamic node with seed nodes: ${seedNodes.mkString(", ")}")
+
+      info(s"Configuring dynamically $host:$port in cluster '$name'")
+
+      clusterConfig(name, config, host, port, seedNodes)
+    }
+
+    ActorSystem(name, createClusterConfig)
+  }
+
+  def clusterSeedNodes(name: String, config: Config): Seq[String] = Try {
+    config.getList("akka.cluster.seed-nodes") map {
+      _.atKey("seed-node")
+    } map { seedNode =>
+      val host = seedNode.getString("seed-node.host")
+      val port = seedNode.getInt("seed-node.port")
+      s""""akka.tcp://$name@$host:$port""""
+    }
+  } getOrElse {
+    config getStringList "akka.cluster.seed-nodes" map { s => s""""$s"""" }
+  }
+
+  def clusterConfig(name: String, config: Config, host: String, port: Int, seedNodes: Seq[String]): Config = ConfigFactory.parseString(s"""
+    akka {
+      actor {
+        provider = "akka.cluster.ClusterActorRefProvider"
+      }
+
+      remote {
+        enabled-transports = ["akka.remote.netty.tcp"]
+        log-remote-lifecycle-events = off
+
+        netty {
+          tcp {
+            hostname = "$host"
+            port = $port
+          }
+        }
+      }
+
+      cluster {
+        seed-nodes = [
+          ${seedNodes mkString ", "}
+        ]
+
+        min-nr-of-members = 2
+        auto-down-unreachable-after = 30s
+
+        metrics {
+          enabled = off
+        }
+      }
+
+      extensions = [
+        "akka.cluster.pubsub.DistributedPubSub",
+        "akka.cluster.metrics.ClusterMetricsExtension"
+      ]
+    }""").withFallback(config)
 }
--- src/test/scala/uk/gov/homeoffice/akka/cluster/ClusterActorSystemSpec.scala
+++ src/test/scala/uk/gov/homeoffice/akka/cluster/ClusterActorSystemSpec.scala
@@ -1,25 +1,149 @@
 package uk.gov.homeoffice.akka.cluster
 
+import java.util.UUID
+import scala.collection.JavaConversions._
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.cluster.Cluster
+import akka.cluster.ClusterEvent._
 import com.typesafe.config.ConfigFactory
+import org.specs2.concurrent.ExecutionEnv
+import org.specs2.execute.{AsResult, Result}
+import org.specs2.matcher.MatchResult
 import org.specs2.mutable.Specification
+import uk.gov.homeoffice.akka.ActorSystemSpecification
+import uk.gov.homeoffice.io.Network
+import uk.gov.homeoffice.specs2._
 
-class ClusterActorSystemSpec extends Specification {
-  "Cluster actor system" should {
-    "not start up with only 1 node" in {
-      todo
+class ClusterActorSystemSpec(implicit ev: ExecutionEnv) extends Specification with ActorSystemSpecification with Network {
+  sequential // Simply because each example can slow each down when running in parallel
+
+  def awaiting[T]: Future[MatchResult[T]] => Result = { _.awaitFor(10 seconds) }
+
+  trait Context extends ActorSystemContext {
+    private var actorSystems = Seq.empty[ActorSystem]
+
+    override def around[R: AsResult](r: => R): Result = {
+      val result = super.around(r)
+      actorSystems foreach { _.terminate() }
+      result
     }
 
-    "start up with 2 nodes" in {
-      val config = ConfigFactory.load("application.test.conf")
+    def clusterActorSystems(numberOfNodes: Int): (Cluster, Seq[ActorSystem]) = {
+      require(numberOfNodes > 0)
 
-      val system1 = ClusterActorSystem("system-1", config, 1)
-      val system2 = ClusterActorSystem("system-2", config, 2)
+      freeport() { port1 =>
+        freeport() { port2 =>
+          freeport() { port3 =>
+            // TODO Configurable number of nodes
+            val config = ConfigFactory.parseString(s"""
+              akka {
+                cluster {
+                  auto-down-unreachable-after = 5s
 
-      ok
+                  seed-nodes = [{
+                    host = "127.0.0.1"
+                    port = $port1
+                  }, {
+                    host = "127.0.0.1"
+                    port = $port2
+                  }, {
+                    host = "127.0.0.1"
+                    port = $port3
+                  }]
+                }
+              }""")
+
+            val actorSystemName = s"${UUID.randomUUID()}-test-actor-system"
+
+            val clusterActorSystems = (1 to numberOfNodes) map { node =>
+              val actorSystem = ClusterActorSystem(actorSystemName, config, node)
+              actorSystems = actorSystems :+ actorSystem
+
+              // Assert all configured nodes for cluster (even though we may not start them all up).
+              val seedNodes = actorSystem.settings.config.getList("akka.cluster.seed-nodes")
+
+              seedNodes.toList.map(_.atKey("seed-node").getString("seed-node")) must beLike {
+                case List(node1, node2, node3) =>
+                  node1 mustEqual s"akka.tcp://${actorSystem.name}@127.0.0.1:$port1"
+                  node2 mustEqual s"akka.tcp://${actorSystem.name}@127.0.0.1:$port2"
+                  node3 mustEqual s"akka.tcp://${actorSystem.name}@127.0.0.1:$port3"
+              }
+
+              actorSystem
+            }
+
+            val cluster = Cluster(clusterActorSystems(0))
+
+            val listeningActor = clusterActorSystems(0) actorOf Props {
+              new Actor {
+                override def receive: Receive = {
+                  case m: MemberEvent => testActor ! m
+                }
+              }
+            }
+
+            cluster.subscribe(subscriber = listeningActor, to = classOf[MemberEvent])
+
+            (cluster, clusterActorSystems)
+          }
+        }
+      }
+    }
+  }
+
+  "Cluster actor system" should {
+    "not start up with only 1 node" in new Context {
+      val (cluster, Seq(_)) = clusterActorSystems(1)
+
+      expectMsgType[MemberJoined](30 seconds)
+      expectNoMsg(30 seconds)
+    }
+
+    "start up with 2 nodes" in new Context {
+      val (cluster, Seq(_, _)) = clusterActorSystems(2)
+
+      twice {
+        expectMsgType[MemberJoined](30 seconds)
+      }
+
+      twice {
+        expectMsgType[MemberUp](30 seconds)
+      }
+    }
+
+    "non seed node joins" in new Context {
+      val (cluster, Seq(actorSystem, _)) = clusterActorSystems(2)
+
+      twice {
+        expectMsgType[MemberJoined](30 seconds)
+      }
+
+      twice {
+        expectMsgType[MemberUp](30 seconds)
+      }
+
+      val extraActorSystem = freeport() { port =>
+        ClusterActorSystem(actorSystem.name, actorSystem.settings.config, host = "127.0.0.1", port = port)
+      }
+
+      expectMsgType[MemberJoined](30 seconds)
     }
 
     "start up with 3 nodes" in {
       todo
+    }
+
+    "Giving same node... what happens?" in {
+      skipped
+
+      val config = ConfigFactory.load("application-2-nodes.test.conf")
+
+      val node1ActorSystem = ClusterActorSystem("test-system", config, 1)
+      val node2ActorSystem = ClusterActorSystem("test-system", config, 1)
+
+      ko
     }
   }
 }
